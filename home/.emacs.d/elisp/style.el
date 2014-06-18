80a699bddbba8fe6d72b82407321ee104fefea1c
Nik Nyby <nnyby@columbia.edu>
2014-06-18 16:38:45 -0400

    add xmobar

--- home/.emacs.d/elisp/style.el
+++ home/.emacs.d/elisp/style.el
@@ -4,6 +4,9 @@
 (set-face-attribute 'default nil :height 120)
 (setq menu-bar-mode nil)
 
+; TODO: only define this when neep is installed
+(set-face-font 'default "-jmk-neep-medium-r-*-*-20-*-*-*-*-*-*-1")
+
 (when (require 'color-theme-sanityinc-tomorrow nil 'noerror)
   (color-theme-sanityinc-tomorrow-blue)
   )
--- home/.xmonad/xmonad.hs
+++ home/.xmonad/xmonad.hs
@@ -1,6 +1,10 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
 
-main = xmonad $ defaultConfig
-    { modMask = mod4Mask
+main = xmonad =<< xmobar defaultConfig
+    { borderWidth = 4
+    , focusedBorderColor = "pink"
+    , modMask = mod4Mask
+    , normalBorderColor = "black"
     , terminal = "urxvtc"
     }
