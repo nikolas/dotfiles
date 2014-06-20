ed42bfcde1cb96f08b946bf4321d771ab350027d
Nik Nyby <nnyby@columbia.edu>
2014-06-20 15:24:44 -0400

    goto-line

--- home/.emacs.d/elisp/keys.el
+++ home/.emacs.d/elisp/keys.el
@@ -1,7 +1,6 @@
 ;; key bindings
 
-(global-set-key "\C-cg" 'magit-status)
-(global-set-key "\C-cl" 'goto-line)
+(global-set-key "\C-cg" 'goto-line)
 (global-set-key "\C-ce" 'edebug-defun)
 (when (require 'ember-rails nil 'noerror)
   (global-set-key "\C-c,e" 'ember-rails-toggle-file)
