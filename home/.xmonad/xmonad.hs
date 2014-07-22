52b628feb5317a7f537facb578d0900b2572d2ba
Nik Nyby <nnyby@columbia.edu>
2014-07-22 09:54:08 -0400

    trying to get spawnOnce working

--- home/.xmonad/xmonad.hs
+++ home/.xmonad/xmonad.hs
@@ -1,11 +1,14 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Util.SpawnOnce
 
-main = xmonad =<< xmobar (ewmh $ defaultConfig)
-    { borderWidth = 4
-    , focusedBorderColor = "pink"
-    , modMask = mod4Mask
-    , normalBorderColor = "black"
-    , terminal = "urxvtc"
-    }
+main = xmonad $ ewmh defaultConfig{
+  borderWidth = 4
+  , focusedBorderColor = "pink"
+  , modMask = mod4Mask
+  , normalBorderColor = "black"
+  , terminal = "urxvtc"
+  , startupHook = do
+    spawnOnce "xmobar ~/.xmobarrc"
+  }
