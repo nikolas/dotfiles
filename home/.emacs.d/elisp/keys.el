0c892a3702c7be5712d10c6abdff9211b2c6a6d1
Nik Nyby <niknyby@riseup.net>
2014-08-18 23:06:09 -0400

    delete-indentation, like vim J

--- home/.emacs.d/elisp/keys.el
+++ home/.emacs.d/elisp/keys.el
@@ -2,6 +2,12 @@
 
 (global-set-key "\C-cg" 'goto-line)
 (global-set-key "\C-ce" 'edebug-defun)
+
+(defun my-delete-indentation ()
+  (interactive)
+  (delete-indentation t))
+(global-set-key "\M-j" 'my-delete-indentation)
+
 (when (require 'ember-rails nil 'noerror)
   (global-set-key "\C-c,e" 'ember-rails-toggle-file)
   )
