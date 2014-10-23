e33b0b64ea1863b1ecd8f78cb9fe9345d68e77b7
Nik Nyby <niknyby@riseup.net>
2014-10-23 04:28:31 -0400

    emacs - only load X styles in X

--- home/.emacs.d/elisp/style.el
+++ home/.emacs.d/elisp/style.el
@@ -1,8 +1,8 @@
-(set-frame-parameter (selected-frame) 'alpha '(90 50))
-(add-to-list 'default-frame-alist '(alpha 90 50))
-
 (setq menu-bar-mode nil)
 
-; TODO: only define this when neep is installed
-;(set-face-font 'default "-jmk-neep-bold-*-*-*-13-120-*-*-*-*-*-*")
-(set-face-attribute 'default nil :height 120)
+(when window-system
+  (set-frame-parameter (selected-frame) 'alpha '(90 50))
+  (add-to-list 'default-frame-alist '(alpha 90 50))
+  ; TODO: only define this when neep is installed
+  (set-face-font 'default "-jmk-neep-bold-*-*-*-13-120-*-*-*-*-*-*")
+  (set-face-attribute 'default nil :height 120))
