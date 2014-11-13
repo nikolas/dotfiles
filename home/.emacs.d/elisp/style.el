a4e456a3b477a449acc0526d7f1370ce459afabd
Nik Nyby <niknyby@riseup.net>
2014-11-13 00:05:29 -0500

    remove transparency

--- home/.emacs.d/elisp/style.el
+++ home/.emacs.d/elisp/style.el
@@ -1,8 +1,8 @@
 (setq menu-bar-mode nil)
 
 (when window-system
-  (set-frame-parameter (selected-frame) 'alpha '(90 50))
-  (add-to-list 'default-frame-alist '(alpha 90 50))
+  ;(set-frame-parameter (selected-frame) 'alpha '(100 50))
+  ;(add-to-list 'default-frame-alist '(alpha 100 50))
   ; TODO: only define this when neep is installed
   (set-face-font 'default "-jmk-neep-bold-*-*-*-13-120-*-*-*-*-*-*")
   (set-face-attribute 'default nil :height 120))
