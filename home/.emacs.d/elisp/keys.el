9bda85ecabc7d2ada78172704e324896de6ef9e0
Nik Nyby <niknyby@riseup.net>
2014-11-19 23:24:05 -0500

    pwd

--- home/.emacs.d/elisp/keys.el
+++ home/.emacs.d/elisp/keys.el
@@ -2,6 +2,7 @@
 
 (global-set-key "\C-x\C-d" 'dired)
 (global-set-key "\C-xd" 'dired)
+(global-set-key "\C-xp" 'pwd)
 (global-set-key "\C-cg" 'goto-line)
 (global-set-key "\C-ce" 'edebug-defun)
 (global-set-key "\C-cr" 'revert-buffer)
