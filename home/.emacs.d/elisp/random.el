afe7d602e11a79b76856d8891f0e25e9934df9e9
Nik Nyby <nnyby@columbia.edu>
2015-08-17 10:32:02 -0400

    add wrap-html-tag

--- home/.emacs.d/elisp/random.el
+++ home/.emacs.d/elisp/random.el
@@ -1,7 +1,26 @@
 (defun kill-other-buffers ()
-      "Kill all other buffers."
-      (interactive)
-      (mapc 'kill-buffer (delq (current-buffer) (buffer-list))))
+  "Kill all other buffers."
+  (interactive)
+  (mapc 'kill-buffer (delq (current-buffer) (buffer-list))))
 
 (define-abbrev global-abbrev-table "pdb"
   "from ipdb import set_trace; set_trace()")
+
+(defun wrap-html-tag (tagName)
+  "Add a tag to beginning and ending of current word or text selection."
+  (interactive "sEnter tag name: ")
+  (let (p1 p2 inputText)
+    (if (use-region-p)
+        (progn
+          (setq p1 (region-beginning) )
+          (setq p2 (region-end) )
+          )
+      (let ((bds (bounds-of-thing-at-point 'symbol)))
+        (setq p1 (car bds) )
+        (setq p2 (cdr bds) ) ) )
+
+    (goto-char p2)
+    (insert "</" tagName ">")
+    (goto-char p1)
+    (insert "<" tagName ">")
+    ))
