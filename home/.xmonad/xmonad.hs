2ad82c943d3bc47d7a3f1650b9fdaf5e86acc3a5
Nik Nyby <nikolas@gnu.org>
2015-11-30 20:04:44 -0500

    update xmonad.hs

--- home/.xmonad/xmonad.hs
+++ home/.xmonad/xmonad.hs
@@ -1,18 +1,38 @@
 import XMonad
-import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Layout.NoBorders
+import XMonad.Util.EZConfig
 
 myManageHook = composeAll
-  [ className =? "pidgin" --> doFloat
-  , className =? "Pidgin" --> doFloat
-  , className =? "Gimp" --> doFloat
-  , resource  =? "desktop_window" --> doIgnore ]
+    [ className =? "pidgin" --> doFloat
+    , className =? "Pidgin" --> doFloat
+    , className =? "Gimp" --> doFloat
+    , resource  =? "desktop_window" --> doIgnore ]
 
-main = xmonad $ ewmh defaultConfig{
-  borderWidth = 3
+myLayout = smartBorders (tiled ||| Mirror tiled ||| Full)
+  where
+    -- default tiling algorithm partitions the screen into two panes
+    tiled = Tall nmaster delta ratio
+
+    -- The default number of windows in the master pane
+    nmaster = 1
+
+    -- Default proportion of screen occupied by master pane
+    ratio = 1 / 2
+
+    -- Percent of screen to increment by when resizing panes
+    delta = 3 / 100
+
+main = xmonad $ ewmh $ defaultConfig {
+  borderWidth = 2
   , focusedBorderColor = "pink"
   , modMask = mod4Mask
   , normalBorderColor = "black"
   , terminal = "urxvtc"
   , manageHook = myManageHook
-  }
+  , layoutHook = myLayout
+  } `additionalKeys`
+       [
+         -- hide/show window borders thanks to http://xmonad.org/xmonad-docs/xmonad-contrib/XMonad-Actions-NoBorders.html
+         -- ((mod4Mask,  xK_g ),   withFocused toggleBorder)
+       ]
