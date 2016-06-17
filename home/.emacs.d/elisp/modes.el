5bc5f58b9e574ae5e3510bd31b87be3965154693
Nik Nyby <nikolas@gnu.org>
2016-06-16 21:53:31 -0400

    add jsx syntax

--- home/.emacs.d/elisp/modes.el
+++ home/.emacs.d/elisp/modes.el
@@ -105,3 +105,12 @@
   (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
   (setq web-mode-engines-alist
         '(("django"    . "\\.html\\'"))))
+
+
+(when (require 'web-mode nil 'noerror)
+  (add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+  (defadvice web-mode-highlight-part (around tweak-jsx activate)
+    (if (equal web-mode-content-type "jsx")
+        (let ((web-mode-enable-part-face nil))
+          ad-do-it)
+      ad-do-it)))
