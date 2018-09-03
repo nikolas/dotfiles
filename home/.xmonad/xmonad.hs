b1d51d3ccbf4ef3768b05e311f347fbbb4341471
Nikolas Nyby <nikolas@gnu.org>
2018-09-03 01:03:06 -0400

    add xmobar

--- home/.xmonad/xmonad.hs
+++ home/.xmonad/xmonad.hs
@@ -1,6 +1,7 @@
 import XMonad
 import XMonad.Actions.NoBorders
 import XMonad.Actions.Volume
+import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Layout.NoBorders
 import XMonad.Layout.DecorationMadness
@@ -34,20 +35,22 @@ myLayout = smartBorders (circleSimpleTabbed ||| tiled ||| Full)
     -- Percent of screen to increment by when resizing panes
     delta = 3 / 100
 
-main = xmonad $ ewmh $ defaultConfig {
+myConfig = defaultConfig {
   borderWidth = 2
   , focusedBorderColor = "pink"
   , modMask = mod4Mask
   , normalBorderColor = "black"
-  , terminal = "urxvtc"
+  , terminal = "urxvt"
   , manageHook = myManageHook
   , layoutHook = myLayout
   } `additionalKeys`
-       [
-         ((mod4Mask, xK_g), withFocused toggleBorder),
+           [
+             ((mod4Mask, xK_g), withFocused toggleBorder),
          ((mod4Mask, xK_F1), manPrompt def),
          ((mod4Mask, xK_F7), toggleMute    >> return ()),
          ((mod4Mask, xK_F8 ), lowerVolume 3 >> return ()),
          ((mod4Mask, xK_F9 ), raiseVolume 3 >> return ()),
          ((mod4Mask, xK_F10), toggleMute    >> return ())
        ]
+
+main = xmonad =<< xmobar myConfig
