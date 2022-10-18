03adb6a72bf7304a4208fd9bac1845e5125228a1
Nikolas Nyby <nikolas@gnu.org>
2022-10-18 15:32:31 -0400

    comment out some neep stuff :(

--- home/.emacs.d/elisp/style.el
+++ home/.emacs.d/elisp/style.el
@@ -3,12 +3,12 @@
 ; Set background color for highlight face to red
 (set-face-attribute 'highlight nil :background "#ff0000")
 
-(when window-system
-  ;(set-frame-parameter (selected-frame) 'alpha '(100 50))
-  ;(add-to-list 'default-frame-alist '(alpha 100 50))
-  ; TODO: only define this when neep is installed
-  (set-face-font 'default "-jmk-neep-bold-*-*-*-13-120-*-*-*-*-*-*")
-  (set-face-attribute 'default nil :height 120))
+;(when window-system
+  ;;(set-frame-parameter (selected-frame) 'alpha '(100 50))
+  ;;(add-to-list 'default-frame-alist '(alpha 100 50))
+  ;; TODO: only define this when neep is installed
+  ;(set-face-font 'default "-jmk-neep-bold-*-*-*-13-120-*-*-*-*-*-*")
+  ;(set-face-attribute 'default nil :height 120))
 
 ;; (custom-set-variables
 ;;  '(custom-enabled-themes (quote (tsdh-dark)))
