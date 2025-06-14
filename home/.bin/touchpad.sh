7a52b9e6e466f5ff2de35c4f1a2176c6cd26f3ee
Nikolas Nyby <nikolas@gnu.org>
2025-06-14 15:11:58 -0400

    random updates

--- home/.bin/touchpad.sh
+++ home/.bin/touchpad.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 # Disable tap to click
-synclient MaxTapTime=0
+synclient MaxTapTime=0 MaxTapMove=0 MaxDoubleTapTime=0 SingleTapTimeout=0 TapAndDragGesture=0
+xinput set-prop 11 360 0
