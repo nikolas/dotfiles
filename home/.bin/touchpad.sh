d3f92443e0a81ab3bb4d4bcef564e468ed6428ed
Nikolas Nyby <nikolas@gnu.org>
2025-06-14 21:02:58 -0400

    touchpad fix

--- home/.bin/touchpad.sh
+++ home/.bin/touchpad.sh
@@ -2,4 +2,4 @@
 
 # Disable tap to click
 synclient MaxTapTime=0 MaxTapMove=0 MaxDoubleTapTime=0 SingleTapTimeout=0 TapAndDragGesture=0
-xinput set-prop 11 360 0
+xinput set-prop 11 "Synaptics Tap Time" 0
