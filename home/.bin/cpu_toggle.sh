45adaa6ef696b090ce0fd00b0be6a3d7f1f90843
Nikolas Nyby <nikolas@gnu.org>
2026-07-10 19:37:20 -0400

    Update cpu_toggle

--- home/.bin/cpu_toggle.sh
+++ home/.bin/cpu_toggle.sh
@@ -3,7 +3,9 @@
 current=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
 
 if [ "$current" = "performance" ]; then
+    echo "Setting powersave"
     sudo cpupower frequency-set -g powersave
 else
+    echo "Setting performance"
     sudo cpupower frequency-set -g performance
 fi
